a72f65684f4e2dd08819ab86b3fa61949fc8ac02
ChloeChen <chloechen727@gmail.com>
2015-01-29 18:25:42 +0800

    Refactor the doc of _wait_visible()

--- pyuia/pageobject.py
+++ pyuia/pageobject.py
@@ -219,8 +219,17 @@ class PageObject(object):
                     (locators, check_visibility, time.time() - start_time))
 
     def _wait_visible(self, locators, timeout_warn=None, handlers=None, timeout=None):
-        """Defalt timeout_warn is assigned by _WARN_TIMEOUT
-        Defalt timeout is assigned by _WAIT_TIMEOUT"""
+       """Wait for all elements which turn into being visible.
+
+        Args:
+            locators: The locators of the elements which we want to wait for.
+            handlers: The handlers which can handle the some specific events during the waiting period.
+            timeout_warn: A warning time for the page loading. The defaut value is _WARN_TIMEOUT.
+            timeout: A timeout for the page loading. The defaut value is _WAIT_TIMEOUT.
+
+        Raises:
+            TimeoutError: An error occurred when the timeout period expired.
+        """
         if not timeout_warn: 
             timeout_warn = self._WARN_TIMEOUT
         if not timeout:
